7cb5b905014e5036310f7f38632c8d8881f44208
Yut-MtGLD <yutkanayama@japanet.co.jp>
2024-11-29 10:16:04 +0900

    Add files via upload

---
+++ spring_tutorial/src/main/java/com/example/springtutorial/repository/UserRepository.java
@@ -0,0 +1,20 @@
+package com.example.springtutorial.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.springtutorial.entity.User;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+	// ユーザー名で完全一致検索
+	List<User> findByUserName(String userName);
+}
+
+/* 
+ * @Queryアノテーションを使用した場合の記述
+ * 使用メソッド⇒searchUser()
+ * 
+ * @Query("SELECT u FROM User u WHERE u.userName = userName")
+ * List<User> searchUser(String userName);
+ */
